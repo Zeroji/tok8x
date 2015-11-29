98c9870cb084589ed629ee5fdf6dcc373d3e1404
shmibs <shmibs@gmail.com>
2015-11-29 00:28:33 -0700

    add partially-completed header-related files

---
+++ src/header.h
@@ -0,0 +1,10 @@
+#ifndef TOK8X_HEADER_H
+#define TOK8X_HEADER_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "buf.h"
+
+buf_t* header_pack_buf(buf_t *bin, char *name, bool archived);
+
+#endif
